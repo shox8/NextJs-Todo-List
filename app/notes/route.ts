b7ea73d4f0f11125060a8f910f6e947fce37eb62
Shoxruh <codershoxruh@gmail.com>
2024-03-12 22:01:48 +0500

    Edited

--- app/notes/route.ts
+++ app/notes/route.ts
@@ -1,14 +1,13 @@
-import { NextRequest } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import { json } from "../_libs";
 
 export let notes: any[] = [];
 
-export async function GET(_request: NextRequest) {
+export async function GET(_request: NextApiRequest) {
   return new Response(json(notes));
 }
 
-export async function POST(request: NextRequest) {
-  const data = await request.json();
-  notes.push(data);
-  return new Response(json(data));
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  notes.push(req.body);
+  return res.status(201).json(req.body);
 }
